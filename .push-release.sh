08ff29271790a3ab23dea36fd651fee5bb4315c3
Ian Duffy <ian@ianduffy.ie>
2015-06-27 04:13:11 +0100

    add push script

---
+++ .push-release.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git config --global user.name "Travis-CI"
+git config --global user.email "travisci@ianduffy.ie"
+git remote set-url origin https://${GH_TOKEN}@github.com/imduffy15/DCUTT-UI.git
+git fetch --tags
+
+export VERSION=`grep -Po '(?<="version": ")[^"]*' package.json`
+
+if [ -z "${TRAVIS_TAG}" ]; then git tag $VERSION; fi
+if [ -z "${TRAVIS_TAG}" ]; then git push --tags; fi
+
+if [ -z "${TRAVIS_TAG}" ]; then cd www; git init; git remote add origin https://${GH_TOKEN}@github.com/imduffy15/DCUTT-UI.git; git add .; git commit -a -m "init"; git push -u origin -f master:gh-pages; cd ../; fi
